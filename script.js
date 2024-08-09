cea41fca9c27a5bc4ccc4a680dfcd22e08cceb01
Aadish Samir <aadish.samir@giis.edu.sg>
2024-08-09 17:29:13 +0800

    fixed sw

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 // Register the service worker
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('service-worker.js').then(registration => {
+        navigator.serviceWorker.register('pwabuilder-sw.js').then(registration => {
             console.log('Service Worker registered with scope:', registration.scope);
         }).catch(error => {
             console.log('Service Worker registration failed:', error);
