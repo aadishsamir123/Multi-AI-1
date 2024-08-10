9a65111d6a425ca319cfd7fe51bd10e303bb52a2
Aadish Samir <aadish.samir@giis.edu.sg>
2024-08-10 11:38:36 +0800

    updated serviceworker

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -2,6 +2,7 @@
 
 const CACHE = "pwabuilder-offline";
 
+importScripts("https://progressier.app/9CEI7z6N3ipSWbJSMkc4/sw.js" );
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 self.addEventListener("message", (event) => {
