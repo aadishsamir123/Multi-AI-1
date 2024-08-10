6622da3b76c198244c70e473ad027a25ea871747
Aadish Samir <aadish.samir@giis.edu.sg>
2024-08-10 14:08:52 +0800

    removed everything related to progressier

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -2,7 +2,6 @@
 
 const CACHE = "pwabuilder-offline";
 
-importScripts("https://progressier.app/9CEI7z6N3ipSWbJSMkc4/sw.js" );
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 self.addEventListener("message", (event) => {
